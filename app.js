47a55cfdbd7fccf6117a970e58e0a3bffaf64957
akshayaggarwal2509 <aggarwalakshay2509@gmail.com>
2023-05-12 01:34:08 +0530

    Change port and add start command

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser=require('body-parser')
 const mysql=require('mysql')
 require('dotenv').config()
 
-const PORT=3000
+const PORT=process.env.PORT||3000
 
 const connection=mysql.createConnection({
     host: process.env.HOST,
